e3a250d64c920c4089314126f8cf104cff569156
Yash Sharma <yash.sharma@zeuslearning.mumbai>
2026-07-21 11:45:10 +0530

    Fix: filestorage config-key typo

--- Services/FileStorageService.cs
+++ Services/FileStorageService.cs
@@ -1,19 +1,15 @@
-using SubmissionProcessor.Worker.Data;
-
 namespace SubmissionProcessor.Worker.Services;
 
 public class FileStorageService : IFileStorageService
 {
     public readonly string _storageRoot;
-    private readonly AppDbContext _context;
     public readonly ILogger<FileStorageService> _logger;
 
-    public FileStorageService(IConfiguration configuration, ILogger<FileStorageService> logger, AppDbContext context)
+    public FileStorageService(IConfiguration configuration, ILogger<FileStorageService> logger)
     {
-        _storageRoot = configuration["FileStorageSettings: StorageRoot"] ?? "./Storage";
+        _storageRoot = configuration["FileStorageSettings:StorageRoot"] ?? "./Storage";
         Directory.CreateDirectory(_storageRoot);
         _logger = logger;
-        _context = context;
     }
 
     public string getFullPath(string fileName)
